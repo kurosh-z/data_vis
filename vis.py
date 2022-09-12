a8f03251cb89b01a278a311ba619e7c53b06938b
kurosh-z <kurosh.zamany@gmail.com>
2022-09-12 09:40:51 +0200

    Visaualization of NB-Lora Data

--- vis.py
+++ vis.py
@@ -5,13 +5,20 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 import requests as rq
+import ptvsd
 
 # fileDay = "26_06_2022"
 
+# print("Waiting for debugger attach")
 
-PHONE_NAME = "sonimKstVoda"
+# ptvsd.enable_attach(address=("localhost", 8501), redirect_output=True)
+# ptvsd.wait_for_attach()
 
-st.title("Data Visualization for Bad Oldesloe")
+# PHONE_NAME = "sonimKstVoda"
+
+PLOT_MODE = ""  # "SMARTPHONE"  or "AIRBOSS"
+
+st.title("Data Visualization of Test Data")
 
 st.markdown(
     """### Quick guide:
@@ -23,51 +30,105 @@ st.markdown(
 )
 
 st.markdown("### Enter the date in dd_mm_yyyy format (eg. 16_07_2022): ")
-fileDay = st.text_input("", "")
+fileDay = st.text_input("date", "09_09_2022", key="date")
+
+st.markdown("### Enter the device ID (eg. AIRBLEAD0 or sonimKstVoda): ")
+DEVICE_NAME = st.text_input("deviceID", "AIRBLEAD0", key="devID")
+
+url = ""
+if DEVICE_NAME[0:8] == "AIRBLEAD":
+    url = f"http://212.227.175.162/smartphone_hub_app/airboss_{fileDay}.txt"
+    PLOT_MODE = "AIRBOSS"
+else:
+    url = f"http://212.227.175.162/smartphone_hub_app/sim_{fileDay}.csv"
+    PLOT_MODE = "SMARTPHONE"
+
+
 # fileName = f"data/sim_{fileDay}.csv"
 
 
 def main():
-    url = f"http://212.227.175.162/smartphone_hub_app/sim_{fileDay}.csv"
+
+    if url == "" or PLOT_MODE == "":
+        st.markdown(f"""dev_name or date is not valid!""")
+        return
+
+    # url = f"http://212.227.175.162/smartphone_hub_app/sim_{fileDay}.csv"
 
     payload = {}
     headers = {}
 
     response = rq.request("GET", url, headers=headers, data=payload)
+    breakpoint()
 
     # with open(fileName, "r") as f:
     #     lines = f.readlines()
 
     if not response.status_code == 200:
         st.markdown(f"""Error: Could not find the file sim_{fileDay}.csv in our database!""")
+        return
 
+    if PLOT_MODE == "SMARTPHONE":
+        df = create_smartphone_df(response)
+    elif PLOT_MODE == "AIRBOSS":
+        df = create_airboss_df(response)
+
+    if len(df) == 0:
+        st.markdown("#### There is no valid data for this day!")
     else:
+        create_plots(df)
 
-        lines = response.text.split("\n")
-        data = {"pc": [], "datetime": [], "name": [], "lat": [], "lon": [], "loc_validity": []}
-        for line in lines:
-            if len(line) == 0:
-                continue
-            l = line.strip()
-            llist = l.split(" ")
-            name = llist[4]
-            if name == PHONE_NAME:
-                data["name"].append(name)
-                data["pc"].append(int(llist[1]))
-                data["datetime"].append(formatTime(llist[2], llist[3]))
-                lat = float(llist[5])
-                lon = float(llist[6])
-                data["lat"].append(lat)
-                data["lon"].append(lon)
-                loc_validity = 0 if lat == 0 and lon == 0 else 1
-                data["loc_validity"].append(loc_validity)
 
-        df = pd.DataFrame.from_dict(data)
+## FUNCTIONS:
 
-        if len(df) == 0:
-            st.markdown("#### There is no valid data for this day!")
-        else:
-            create_plots(df)
+
+def create_airboss_df(response):
+    lines = response.text.split("\n")
+    data = {"pc": [], "datetime": [], "name": [], "lat": [], "lon": [], "loc_validity": [], "pressure": []}
+    for line in lines:
+        if len(line) == 0:
+            continue
+        l = line.strip()
+        llist = l.split(",")
+        name = llist[3]
+        if name == DEVICE_NAME:
+            data["name"].append(name)
+            data["pc"].append(int(llist[0]))
+            data["datetime"].append(formatTime(llist[1], llist[2]))
+            lat = float(llist[4])
+            lon = float(llist[5])
+            data["lat"].append(lat)
+            data["lon"].append(lon)
+            loc_validity = 0 if lat == 0 and lon == 0 else 1
+            data["loc_validity"].append(loc_validity)
+            data["pressure"].append(int(llist[6]))
+
+    df = pd.DataFrame.from_dict(data)
+    return df
+
+
+def create_smartphone_df(response):
+    lines = response.text.split("\n")
+    data = {"pc": [], "datetime": [], "name": [], "lat": [], "lon": [], "loc_validity": []}
+    for line in lines:
+        if len(line) == 0:
+            continue
+        l = line.strip()
+        llist = l.split(" ")
+        name = llist[4]
+        if name == DEVICE_NAME:
+            data["name"].append(name)
+            data["pc"].append(int(llist[1]))
+            data["datetime"].append(formatTime(llist[2], llist[3]))
+            lat = float(llist[5])
+            lon = float(llist[6])
+            data["lat"].append(lat)
+            data["lon"].append(lon)
+            loc_validity = 0 if lat == 0 and lon == 0 else 1
+            data["loc_validity"].append(loc_validity)
+
+    df = pd.DataFrame.from_dict(data)
+    return df
 
 
 def formatTime(date, time):
@@ -101,13 +162,42 @@ def calAvaragePcs(df, seconds=900):
 def create_plots(df):
     df["datetime"] = df["datetime"].astype("datetime64[ns]")
 
-    st.markdown("### Change the duration for the 2nd plot (default 15 minutes)")
-    dur_minutes = st.text_input("", "15")
-    dur_minutes = int(dur_minutes)
-    timeList, avarages = calAvaragePcs(df, seconds=60 * dur_minutes)
+    if PLOT_MODE == "SMARTPHONE":
+        st.markdown("### Change the duration for the 2nd plot (default 15 minutes)")
+        dur_minutes = st.text_input("", "15")
+        dur_minutes = int(dur_minutes)
+        timeList, avarages = calAvaragePcs(df, seconds=60 * dur_minutes)
+    elif PLOT_MODE == "AIRBOSS":
+        st.markdown("### Change the duration for the 2nd plot (default 5 minutes)")
+        dur_minutes = st.text_input("", "5")
+        dur_minutes = int(dur_minutes)
+        timeList, avarages = calAvaragePcs(df, seconds=60 * dur_minutes)
+
     df2 = pd.DataFrame.from_dict({"pc": avarages, "datetime": timeList})
 
-    # fig = go.Figure()
+    if PLOT_MODE == "AIRBOSS":
+        fig_pressure = px.scatter(
+            df,
+            x="datetime",
+            y=df.pressure,
+            labels={
+                "pc": "Number of packets",
+                "pressure": "Pressure of the cylinder",
+                "datetime": "date and time",
+                "index": "packet count",
+                "loc_validity": "location's validity",
+            },
+            # template="plotly_white",
+            template="ggplot2",
+            color_discrete_sequence=["blue"],
+        )
+        fig_pressure.update_layout(title_text=f"<b>Time series of pressure on {fileDay}</b>")
+        fig_pressure.update_layout(
+            font_color="black",
+            # title_font_family="Times New Roman",
+            # title_font_color="black",
+            # legend_title_font_color="green",
+        )
 
     # fig.add_trace(go.Scatter(x=list(df.datetime), y=list(df.pc)))
     fig = px.scatter(
@@ -231,6 +321,13 @@ def create_plots(df):
 
     # fig.show()
     # fig2.show()
+    if PLOT_MODE == "AIRBOSS":
+        st.markdown("----")
+        st.markdown(
+            """### Plot0: Pressure against time
+                """
+        )
+        st.plotly_chart(fig_pressure)
 
     st.markdown("----")
     st.markdown(
